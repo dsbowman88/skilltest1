a5fc242490d2f93684ca34db304a372c5f46023e
Douglas Bowman <d.s.bowman88@gmail.com>
2019-01-27 17:24:22 +0100

    opdracht klaar

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$invoer = file("input.txt");
+$outfile = fopen("output.txt", "w");
+foreach ($invoer as $inv){
+    $aregel = explode(";", $inv);
+    $naam = $aregel[0];
+    $score = (int)$aregel[1];
+    $lijst[] = ['naam' => $naam, 'score' => $score];
+}
+sort($lijst);
+fwrite($outfile, $lijst[0]['naam'] . "; " . $lijst[0]['score'] . "\n");
+fwrite($outfile, $lijst[1]['naam'] . "; " . $lijst[1]['score'] . "\n");
+fwrite($outfile, $lijst[2]['naam'] . "; " . $lijst[2]['score'] . "\n");
+fclose($outfile);
